3f4a9b09e4a8e500d402a6a1dcc988675a8d13cb
Robin Lange <robin.langenc@gmail.com>
2019-03-29 20:09:54 +1100

    Implemented glxinfo parsing

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from pci import list_gpus
+from opengl import get_opengl_info
 
 gpus_pci_map = list_gpus()
 
@@ -7,3 +8,9 @@ for bus_id in gpus_pci_map.keys():
     print("%s %s (%s)" % (gpus_pci_map[bus_id]["vendor"], gpus_pci_map[bus_id]["name"], bus_id))
     print("\tAvailable modules: %s" % ",".join(gpus_pci_map[bus_id]["available_modules"]))
     print("\tModule in use: %s" % gpus_pci_map[bus_id]["active_module"])
+
+opengl_info = get_opengl_info()
+
+print("Renderer: %s" % opengl_info["renderer"])
+print("Renderer version: %s" % opengl_info["renderer_version"])
+print("OpenGL version: %s" % opengl_info["opengl_version"])
---
+++ opengl.py
@@ -0,0 +1,59 @@
+import re
+from bash import exec_bash
+
+
+def get_opengl_info():
+
+    returncode, glxinfo_output, stderr = exec_bash("glxinfo")
+
+    opengl_info = {"error": False, "opengl_version": "",
+                   "renderer": "", "renderer_version": ""}
+
+    if returncode != 0:
+        _print_glxinfo_error("glxinfo returned an error : %s" % stderr)
+        opengl_info["error"] = True
+        return opengl_info
+
+    opengl_version_key = "OpenGL version string:"
+
+    for line in glxinfo_output.splitlines():
+
+        string_index = line.find(opengl_version_key)
+
+        if string_index != -1:
+
+            version_string = line[string_index+len(opengl_version_key):]
+            version_string_items = version_string.split(" ")
+
+            version_string_items = [it.replace(" ", "") for it in version_string_items]
+            version_string_items = [it for it in version_string_items if it != ""]
+
+            if len(version_string_items) != 3:
+                _print_glxinfo_error("%d items in version string" % len(version_string_items))
+                opengl_info["error"] = True
+                return opengl_info
+
+            opengl_version = version_string_items[0]
+            if not re.fullmatch("[0-9\\.]+", opengl_version):
+                _print_glxinfo_error("OpenGL version is \"%s\"" % opengl_version)
+                opengl_info["error"] = True
+                return opengl_info
+            else:
+                opengl_info["opengl_version"] = opengl_version
+
+            renderer = version_string_items[1]
+            opengl_info["renderer"] = renderer
+
+            renderer_version = version_string_items[2]
+            opengl_info["renderer_version"] = renderer_version
+
+            return opengl_info
+
+    else:
+        _print_glxinfo_error("OpenGL version string not found")
+        opengl_info["error"] = True
+        return opengl_info
+
+
+def _print_glxinfo_error(msg):
+    print("ERROR : glxinfo parsing : %s" % msg)
