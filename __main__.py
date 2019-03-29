e1f7f802edf1145e7ca9bc4fb51d27fa9d85162d
Robin Lange <robin.langenc@gmail.com>
2019-03-29 20:33:56 +1100

    Implemented CPU governor detection

--- __main__.py
+++ __main__.py
@@ -1,12 +1,13 @@
 from pci import list_gpus
 from opengl import get_opengl_info
+from cpu import get_cpu_governor_info
 
 gpus_pci_map = list_gpus()
 
 for bus_id in gpus_pci_map.keys():
 
     print("%s %s (%s)" % (gpus_pci_map[bus_id]["vendor"], gpus_pci_map[bus_id]["name"], bus_id))
-    print("\tAvailable modules: %s" % ",".join(gpus_pci_map[bus_id]["available_modules"]))
+    print("\tAvailable modules: %s" % "/".join(gpus_pci_map[bus_id]["available_modules"]))
     print("\tModule in use: %s" % gpus_pci_map[bus_id]["active_module"])
 
 opengl_info = get_opengl_info()
@@ -14,3 +15,8 @@ opengl_info = get_opengl_info()
 print("Renderer: %s" % opengl_info["renderer"])
 print("Renderer version: %s" % opengl_info["renderer_version"])
 print("OpenGL version: %s" % opengl_info["opengl_version"])
+
+cpu_governor_info = get_cpu_governor_info()
+
+print("Available CPU governors: %s" % "/".join(cpu_governor_info["available_governors"]))
+print("Current CPU governor: %s" % cpu_governor_info["current_governor"])
---
+++ cpu.py
@@ -0,0 +1,57 @@
+import os
+
+
+def get_cpu_governor_info():
+
+    cpu_governor_info = {"error": False,  "available_governors": [],
+                         "current_governor": ""}
+
+    available_governors_path = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors"
+    current_governor_path = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+
+    #
+    # Returning available governors
+
+    if not os.path.isfile(available_governors_path):
+        _print_cpu_governor_error("%s does not exist" % available_governors_path)
+        cpu_governor_info["error"] = True
+        return cpu_governor_info
+    else:
+        with open(available_governors_path, "r") as f:
+            available_governors_str = f.read()
+
+        available_governors = available_governors_str.split(" ")
+        available_governors = [g_str.strip() for g_str in available_governors]
+
+        if len(available_governors) == 0:
+            _print_cpu_governor_error("No CPU governors returned by %s" % available_governors_path)
+            cpu_governor_info["error"] = True
+            return cpu_governor_info
+        else:
+            cpu_governor_info["available_governors"] = available_governors
+
+    #
+    # Returning current governor
+
+    if not os.path.isfile(current_governor_path):
+        _print_cpu_governor_error("%s does not exist" % current_governor_path)
+        cpu_governor_info["error"] = True
+        return cpu_governor_info
+    else:
+        with open(current_governor_path, "r") as f:
+            current_governor = f.read()
+
+        current_governor = current_governor.strip()
+
+        if current_governor == "":
+            _print_cpu_governor_error("No CPU governor returned by %s" % current_governor_path)
+            cpu_governor_info["error"] = True
+            return cpu_governor_info
+        else:
+            cpu_governor_info["current_governor"] = current_governor
+
+    return cpu_governor_info
+
+
+def _print_cpu_governor_error(msg):
+    print("ERROR : CPU governor detection : %s" % msg)
