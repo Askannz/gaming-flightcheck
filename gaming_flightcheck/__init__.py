aebd21f00276c3236af93c948aea34bf30bfb61d
Robin Lange <robin.langenc@gmail.com>
2019-04-06 12:55:02 +1100

    Implemented Vulkan ICD listing and vulkaninfo status checking

--- gaming_flightcheck/__init__.py
+++ gaming_flightcheck/__init__.py
@@ -5,7 +5,7 @@ from .info.cpu import get_cpu_governor_info
 from .info.limits import get_limits_info
 from .info.nvidia import get_nvidia_PAT_info
 from .info.display import get_PRIME_sync_info
-from .info.vulkan import get_libvulkan_info
+from .info.vulkan import get_vulkan_info
 from .distribution_specific.ArchlinuxReader import ArchlinuxReader
 
 
@@ -17,14 +17,15 @@ def print_system_info():
     distribution_reader = ArchlinuxReader()
     system_info, checklist = distribution_reader.check_nvidia_packages(system_info, checklist)
 
-    system_info["available_executables"] = get_executables_availability(["ldconfig", "lspci", "glxinfo", "xrandr"])
+    system_info["available_executables"] = \
+        get_executables_availability(["ldconfig", "lspci", "glxinfo", "xrandr", "vulkaninfo"])
     system_info["GPUs_PCI"] = get_GPUs_PCI_info(system_info)
     system_info["opengl"] = get_opengl_info(system_info)
     system_info["cpu_governor"] = get_cpu_governor_info(system_info)
     system_info["limits"] = get_limits_info(system_info)
     system_info["nvidia_PAT"] = get_nvidia_PAT_info(system_info)
     system_info["PRIME_sync"] = get_PRIME_sync_info(system_info)
-    system_info["libvulkan"] = get_libvulkan_info(system_info)
+    system_info["vulkan"] = get_vulkan_info(system_info)
 
     gpus_pci_map = system_info["GPUs_PCI"]["pci_map"]
 
@@ -58,7 +59,7 @@ def print_system_info():
     nvidia_PAT_info = system_info["nvidia_PAT"]
 
     print("")
-    print("Page Attribute Table usage enabled : %s" % ("yes" if nvidia_PAT_info["enabled"] else "no"))
+    print("Nvidia Page Attribute Table usage enabled : %s" % ("yes" if nvidia_PAT_info["enabled"] else "no"))
 
     PRIME_sync_info = system_info["PRIME_sync"]
 
@@ -66,11 +67,20 @@ def print_system_info():
     print("PRIME Sync : supported on %d monitor(s), enabled on %d"
           % (PRIME_sync_info["nb_supported"], PRIME_sync_info["nb_enabled"]))
 
-    libvulkan_info = system_info["libvulkan"]
-
     print("")
-    print("libvulkan installed :\n"
-          "\t32-bit : %s\n"
-          "\t64-bit : %s\n"
+    print("Vulkan :")
+
+    libvulkan_info = system_info["vulkan"]["libvulkan"]
+    print("\tlibvulkan installed :\n"
+          "\t\t32-bit : %s\n"
+          "\t\t64-bit : %s"
           % (("yes" if libvulkan_info["32bit"] else "no"),
              ("yes" if libvulkan_info["64bit"] else "no")))
+
+    ICDs_info = system_info["vulkan"]["ICDs"]
+    print("\tAvailable ICDs :")
+    for file_name in ICDs_info["files_list"]:
+        print("\t\t%s" % file_name)
+
+    vulkaninfo_status_info = system_info["vulkan"]["vulkaninfo_status"]
+    print("\tvulkaninfo error : %s" % ("no" if vulkaninfo_status_info["status_ok"] else "yes"))
--- gaming_flightcheck/info/vulkan.py
+++ gaming_flightcheck/info/vulkan.py
@@ -1,5 +1,49 @@
+import os
+from ..utils.bash import exec_bash
 from .libraries import is_library_installed
 
 
-def get_libvulkan_info(system_info):
+def get_vulkan_info(system_info):
+
+    vulkan_info = {}
+
+    vulkan_info["libvulkan"] = _get_libvulkan_info(system_info)
+    vulkan_info["ICDs"] = _get_ICDs_info(system_info)
+    vulkan_info["vulkaninfo_status"] = _get_vulkaninfo_status_info(system_info)
+
+    return vulkan_info
+
+
+def _get_libvulkan_info(system_info):
     return is_library_installed(system_info, "libvulkan")
+
+
+def _get_ICDs_info(system_info):
+
+    ICDs_info = {"error": False, "files_list": []}
+
+    ICDs_folder_path = "/usr/share/vulkan/icd.d/"
+
+    if not os.path.isdir(ICDs_folder_path):
+        return ICDs_info
+
+    ICDs_info["files_list"] = list(os.listdir(ICDs_folder_path))
+
+    return ICDs_info
+
+
+def _get_vulkaninfo_status_info(system_info):
+
+    assert "available_executables" in system_info.keys()
+
+    vulkaninfo_status_info = {"error": False, "status_ok": False}
+
+    if "vulkaninfo" not in system_info["available_executables"]:
+        vulkaninfo_status_info["error"] = True
+        return vulkaninfo_status_info
+
+    returncode, _, _ = exec_bash("vulkaninfo")
+
+    vulkaninfo_status_info["status_ok"] = (returncode == 0)
+
+    return vulkaninfo_status_info
