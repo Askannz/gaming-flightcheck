abc16105427f758dce95c57d971e9af2289bce6d
Robin Lange <robin.langenc@gmail.com>
2019-05-16 21:51:23 +1000

    Run bash commands with "C" locale to avoid inconsistent output

--- gaming_flightcheck/utils/bash.py
+++ gaming_flightcheck/utils/bash.py
@@ -3,7 +3,7 @@ import subprocess
 
 def exec_bash(command):
 
-    ret = subprocess.run(["bash", "-c", command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ret = subprocess.run(["bash", "-c", "LC_ALL=C %s" % command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout, stderr = ret.stdout.decode('utf-8')[:-1], ret.stderr.decode('utf-8')[:-1]
     returncode = ret.returncode
